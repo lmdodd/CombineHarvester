48aee567a5ce2f14448bc695485963a1bc1fe762
Laura Dodd <ldodd@cern.ch>
2016-02-04 19:02:28 +0100

    Update

--- mssmRun.sh
+++ mssmRun.sh
@@ -25,13 +25,13 @@ pushd $DIR/${LIMITDIR}
 
 for channel in et mt;
 do
-    for signal in ggH;
+    for signal in bbH ggH;
     do
     	pushd $DIR/${LIMITDIR}/${signal}/${channel}
         #80-140:10,140-180:20,250,400,450,500-1000:100,1200,1500,1600,1800,2000,2600,2900,3200
         for mass in 80 90 100 110 120 130 140 160 180 250 400 500 600 700 900 1000 1200 1500 1600 1800 2000 2600 2900 3200;
         do
-            for category in 2; 
+            for category in 2 3; 
             do 
                 combine -M Asymptotic -m ${mass} ${mass}/htt_${channel}_${category}_${ERA}.txt
             done
